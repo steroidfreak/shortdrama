03e351e0d7d2395a3cc91b5ddeee72c0ae48dc24
steroidfreak <134042732+steroidfreak@users.noreply.github.com>
2024-09-27 17:47:53 +0800

    first release

---
+++ data.js
@@ -0,0 +1,152 @@
+const data=[
+    {
+      "title": "《玄學大佬驚豔全球》第1~100集【高清完結合集】丨有沒有要算命的，壹卦八百不准不要錢！我說的都是命數，妳們找我逆天改命就是咯！爲了我們家的命格，身爲老七的我壹定不能讓六哥離婚，嫂嫂我來了！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "YZtdt042Y_M"
+    },
+    {
+      "title": "[MULTI SUB]《天堂没有遗忘》🍒我的女儿被锁在了车里，而她的父亲此时却在陪别人的儿子逛游乐园……#短剧 #shortdrama",
+      "description": "欢迎来到追剧酱的频道~ 分享最新鲜的女频小短剧！   如果你也短剧的忠实观众，欢迎点点订阅哦   声明：本频道短剧内容已获正版 ...",
+      "videoId": "6PFgMZpMZ70"
+    },
+    {
+      "title": "[Multi SUB] ｜全集｜都市修真仙王下凡，人间福祸相依，凡人蝼蚁惹怒仙王，仙王出手现威力！#爽文 #MiniDrama#精彩大陆短剧 #下山追短剧",
+      "description": "正版授权，欢迎订阅，和我一起下山追短剧~ https://bit.ly/4cCnCUG Latest Completed Feel-Good Dramas, Authorized and ...",
+      "videoId": "gKiLHzsQe54"
+    },
+    {
+      "title": "《离婚后我带女儿走向人生巅峰》#短剧 #短劇 #星河劇社",
+      "description": "短剧#短劇#男频#女频#优质小说改编#霸总#灰姑娘#追妻火葬场#大女主逆袭#短剧#精彩短剧#最新短剧全集#精彩短剧推荐#短剧全集 ...",
+      "videoId": "K-_8mS0majY"
+    },
+    {
+      "title": "《慶余年之帝王業》第1~100集【高清完結合集】丨人心險惡，竟還冠以天名！太子要除掉我，而我卻被救下！世子死而複生，定要回京救父！我要改變世道，奪嫡之爭勢在必得，可我忘記屠龍的少年終將成龍！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "NkZQbeK0Irg"
+    },
+    {
+      "title": "直到王爷会想起青楼女子第一次竟有落红，才反应过来女孩肚子的孩子竟是他自己的#微剧吧 #禁欲王爷霸道宠 #因为一个片段看了整部剧 #好剧推荐 #一起追剧",
+      "description": "欢迎来到“在下浮生”频道！ 这里充满了二次元原创沙雕动画的乐趣。 我们的频道特色是结合了动漫热情和沙雕幽默，专门为喜欢听 ...",
+      "videoId": "bJ8bzkFT-xs"
+    },
+    {
+      "title": "[MULTI SUB]《缠她上瘾，陆总的娇气包》翟一莹×黄文博新剧🍒我怀孕了，怀了一个陌生大叔的孩子，他竟然还是我的实习老师！ #短剧 #shortdrama",
+      "description": "欢迎来到追剧酱的频道~ 分享最新鲜的女频小短剧！   如果你也短剧的忠实观众，欢迎点点订阅哦   声明：本频道短剧内容已获正版 ...",
+      "videoId": "H-SXXCl-q7k"
+    },
+    {
+      "title": "《彷徨之刃》#短剧 #短劇 #星河劇社",
+      "description": "短剧#短劇#男频#女频#优质小说改编#霸总#灰姑娘#追妻火葬场#大女主逆袭#短剧#精彩短剧#最新短剧全集#精彩短剧推荐#短剧全集 ...",
+      "videoId": "VUvTvQnkr7M"
+    },
+    {
+      "title": "《玄學大佬驚豔全球》第1~100集【高清完結合集】丨有沒有要算命的，壹卦八百不准不要錢！我說的都是命數，妳們找我逆天改命就是咯！爲了我們家的命格，身爲老七的我壹定不能讓六哥離婚，嫂嫂我來了！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "YZtdt042Y_M"
+    },
+    {
+      "title": "[MULTI SUB]《離婚後三個哥哥獨寵我》渣男出軌還嫌棄她窮酸，沒想到下秒3個大佬哥哥出現認她回豪門！團寵小妹甜入骨！💕#甜寵 #言情 #chinesedrama #短剧",
+      "description": "歡迎訂閲頻道  https://www.youtube.com/channel/UCpGZxK9ePrdmJOquMRA2xrA 【离婚后三个哥哥独宠我】 #热门#大陆短剧#短 ...",
+      "videoId": "pwhgjkXpNfY"
+    },
+    {
+      "title": "【大結局完整版】《極速燃魂》第1~30集#热血#都市#玄幻#亲情 #逆袭#玄幻剧场",
+      "description": "【玄幻剧场】每日更新！致力於為大家提供驚險刺激的短視頻，所有短視頻都是熱門小說改編而成，關註免費看大結局！ #熱播短劇# ...",
+      "videoId": "qWbjA9Ei7Vc"
+    },
+    {
+      "title": "《桃花马上请长缨》1~完结",
+      "description": "糖糖微笑社#空间#古風#穿越重生#种田#甜宠#原創動畫#二次元#劇情#大宋映畫.",
+      "videoId": "zvodFRQxeFA"
+    },
+    {
+      "title": "顶级豪门总裁，竟成了我的小奶狗男友！本小姐可是夜店女王，却是万花丛中过，片叶不沾身！终于遇上昔日白月光，尘封的爱意涌上心头！一吻定情，春风一度爱情生《顶级豪门总裁成了我的金丝雀》#都市爱情 #堆糖",
+      "description": "我们强调了视频内容的合规性，绝对没有任何不良引导或危险行为  「正版授权，持续更新」",
+      "videoId": "fohWXH1QiR0"
+    },
+    {
+      "title": "[MULTI SUB] 《逆行人生之天生继承者》他们都不看好你，偏偏你最争气。天降大财，逆行人生，可我依然一往无前~（高清全集）#短剧 #热门短剧 #短剧推荐 #爱情 #都市 #霸总",
+      "description": "独家授权，请勿侵权，欢迎订阅！】希望大家喜欢我的视频。 #短劇#灰姑娘霸總#追妻火葬場#短劇#灰姑娘霸總#追妻火葬場#追爽 ...",
+      "videoId": "ox4xjBf76aI"
+    },
+    {
+      "title": "[MULTI SUB]Searching for relatives in the mist&amp;团宠老妈惹不起&amp;婚礼上婆婆惊艳全场&amp;铁拳无敌杨芊芊#dramachina#短剧#亲情#新剧#影视#女频",
+      "description": "The plot requires it, please do not imitate~ Welcome to the Aqua Drama Club! Watch the full collection of wonderful short ...",
+      "videoId": "S4EZs3JMVfI"
+    },
+    {
+      "title": "一口氣大結局！《#盲妻看得见》#短劇 #甜寵 #霸道總裁 #愛情 #最火短剧推荐 #短剧推荐 #短剧mcn影响力大赛 #都市情感剧 #霸總 #chinesedrama",
+      "description": "哈嘍，朋友們好~ 這是愛追劇的girl KK   每天更新精彩熱播短劇   #kk追劇girl #最火短剧推荐#短劇全集#甜寵#短劇#drama.",
+      "videoId": "BghYznlyIzg"
+    },
+    {
+      "title": "[MULTI SUB] 💕救人闪婚老公是影帝✨安然善良救了一个老爷子，让其和他孙子闪婚。闪婚后，安然并不知其身份，直到公司上班时，他突然出现，才知道他是当红影帝~#短剧 #drama",
+      "description": "独家授权，请勿侵权，欢迎订阅！】安然善良救了一个老爷子，让其和他孙子闪婚。闪婚后，安然并不知其身份，直到公司上班时， ...",
+      "videoId": "A8Z87grtJAw"
+    },
+    {
+      "title": "周璇 - 許我向你看",
+      "description": "許我向你看作詞：嚴寬作曲：莊宏許我向你看向你看多看一眼我苦守著一個共同的信念今天才回到我的面前許我向你看向你看多看 ...",
+      "videoId": "kU5mFJ0Xbpc"
+    },
+    {
+      "title": "【MultiSub】首富千金隐藏身份嫁救命恩人，势利眼婆婆从中作梗，甚至安排绿茶小三勾引儿子；她恢复大小姐身份与渣男决裂，醒悟后的总裁开启地狱追妻难度《#萧总夫人又要离婚了》#短剧#不顾一切奔向你",
+      "description": "最新精彩短剧每日更新，订阅我们Subscribe Now☞https://shorturl.at/ymQvo ▷▷更多免费短剧More Free Dramas: ...",
+      "videoId": "1pIKJJbxIpM"
+    },
+    {
+      "title": "朱茉颜&amp;王奕然 | 十年暗戀，祁教授他終於娶到了心愛的人！[MULTI SUB | FULL] #MiniDrama #精彩大陆短剧",
+      "description": "中國最新短劇熱播   大家好，歡迎來到  浪漫短劇社   更新優質原創短劇內容， 甜寵，重生，先婚後愛，追妻火葬場... 短劇均為 ...",
+      "videoId": "lg-RtHAOhMk"
+    },
+    {
+      "title": "【大結局完整版】《督軍府的小千金》第1~59全集#古风 #古装 #甜宠 #恋爱 #爱情 #古装剧 #逆袭 #短剧 #神剧课代表",
+      "description": "我们强调了视频内容的合规性，剧情均为戏剧化表达，绝对没有任何不良引导或危险行为，请勿模仿任何剧情中出现的行为正版授權 ...",
+      "videoId": "jF-7jtGfNBI"
+    },
+    {
+      "title": "《壹劍傾城》第1~100集【高清完結合集】丨師傅教我的劍法，竟是武林絕學傾城劍法！但這卻引來了殺生之禍，師傅居然被他師妹背刺！我唯有去找師姐，陰陽雙修大成功法，才能爲師傅報仇雪恨，走向巅峰！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "Z1skYMTpYA4"
+    },
+    {
+      "title": "闪婚老伴是豪门",
+      "description": "每日更新原創短劇，聚焦甜寵，穿越，重生各類題材歡迎訂閱我的頻道，第一時間觀看最新短劇.",
+      "videoId": "_H9RPZG0WxU"
+    },
+    {
+      "title": "《最佳离婚攻略》#短剧 #短劇 #星河劇社",
+      "description": "短剧#短劇#男频#女频#优质小说改编#霸总#灰姑娘#追妻火葬场#大女主逆袭#短剧#精彩短剧#最新短剧全集#精彩短剧推荐#短剧全集 ...",
+      "videoId": "SHMsg3gqu1c"
+    },
+    {
+      "title": "[MULTI SUBS]富家千金愛上窮小子，想為其付出所有，結果卻被他用仇恨報復，天呐！【全集獨播首發】#短劇 #现代 #爱情 《半生夫妻，终成陌路》",
+      "description": "該短劇為南京玄水文化傳播有限公司版权所有，经授权后在本頻道內獨家上傳，除此外均為盜版！ 感謝您對正版內容的支持，不要 ...",
+      "videoId": "w-znxmvxXyA"
+    },
+    {
+      "title": "【FULL】《慕總終於靠娃上位了》 #甜宠 #恋爱 #爱情  #逆袭 #短剧",
+      "description": "灰姑娘霸總#追妻火葬場#romanticshortchinesedrama 每日更新原創短劇，聚焦甜寵，穿越，重生各類題材歡迎訂閱我的頻道，第一 ...",
+      "videoId": "npdTiZpPNT4"
+    },
+    {
+      "title": "【MULTI SUB】《荣耀之下》【短剧合集】#甜宠 #短剧 #都市 #热血 #短剧全集 #重生 #逆袭 #古装 #战斗 #大陆剧 #热门 #热门短剧推荐 #奈奈剧场",
+      "description": "我们强调了视频内容的合规性，绝对没有任何不良引导或危险行为啾咪❤ (We have emphasized the compliance of the video ...",
+      "videoId": "N2MJSi2nYtU"
+    },
+    {
+      "title": "《我就背首詩妳們咋全下跪了》第1~100集【高清完結合集】丨意外穿越修真界，我連壹點靈力都沒有！每天只能背背古詩，沒想到卻獲得兩只靈寵！她們居然是妖皇？而我口含天憲，言出法隨，竟是儒聖轉世！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "EfqE0wqvNFY"
+    },
+    {
+      "title": "💕《不知江月待何人》#nmzl#瑪麗蘇#短劇推薦#霸总",
+      "description": "我们强调了视频内容的合规性，绝对没有任何不良引导或危险行为#恋爱#霸总#短剧#都市   带你追剧爽不停   喜欢就快点关注我！",
+      "videoId": "RoXcgVpWa7c"
+    },
+    {
+      "title": "《重生2000之我用科技拯救全球》第1~120集【高清完結合集】丨二十年的時間，我沒日沒夜的研究可控核聚變！如今終于成功，但家庭卻被我毀了！沒想到我竟重生回到過去，這壹次我不會再留任何遺憾！#嘟嘟剧场",
+      "description": "歡迎訂閱嘟嘟的頻道哦https://www.youtube.com/@DUDUJUCHANG?sub_confirmation=1 獨家正版授權短劇，感謝大家支持！",
+      "videoId": "ggXzZa455tI"
+    }
+  ]
